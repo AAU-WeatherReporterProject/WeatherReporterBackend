ee0896cb3e5e64b7f4ec1c0b4f51c89f2b212f6e
David Laubreiter <david.laubreiter@outlook.com>
2020-12-18 19:42:47 +0100

    Add checkstyle rules

--- src/main/java/at/aau/projects/weatherreporter/rest/TestEndpoint.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/TestEndpoint.java
@@ -1,9 +1,14 @@
 package at.aau.projects.weatherreporter.rest;
 
-import at.aau.projects.weatherreporter.rest.model.TemperatureMeasurement;
 import at.aau.projects.weatherreporter.rest.model.TemperatureData;
+import at.aau.projects.weatherreporter.rest.model.TemperatureMeasurement;
+
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nonnull;
 import java.util.List;
