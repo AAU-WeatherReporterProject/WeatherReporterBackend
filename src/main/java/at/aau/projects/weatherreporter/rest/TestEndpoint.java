a9cc3d9b9b99add2faa47221495a648b15061623
yodiack <pipp.peter@gmx.at>
2020-12-18 19:53:42 +0100

    add dataingest service dependency

--- src/main/java/at/aau/projects/weatherreporter/rest/TestEndpoint.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/TestEndpoint.java
@@ -2,6 +2,7 @@ package at.aau.projects.weatherreporter.rest;
 
 import at.aau.projects.weatherreporter.rest.model.TemperatureMeasurement;
 import at.aau.projects.weatherreporter.rest.model.TemperatureData;
+import at.aau.projects.weatherreporter.rest.service.DataIngestService;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("v1")
 public class TestEndpoint {
-    @PostMapping
-    public void ingest(@RequestBody @Nonnull TemperatureData data) {
-        System.out.println(data);
+
+    private DataIngestService ingestService;
+
+    public TestEndpoint(DataIngestService ingestService )
+    {
+        this.ingestService = ingestService;
     }
 
-    @GetMapping
+    @PostMapping(value = "/temps")
+    public void ingest(@RequestBody @Nonnull TemperatureData data) {
+        ingestService.ingestData(data);
+    }
+
+    @GetMapping(value = "/dataPoints")
     public List<TemperatureMeasurement> getDataPoints(
             @Param("from") String from, @Param("to") String to, @Param("key") String locationKey) {
         return List.of(
