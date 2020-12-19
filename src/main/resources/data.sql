50909c3c05077907b794f5c27a3c7fc63a484e3a
yodiack <pipp.peter@gmx.at>
2020-12-19 12:05:41 +0100

    add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into TEMPERATURE_MEASUREMENT_POINT (measurement_key, name, location) values('123456789','TestLocation','irgendwo'); 
+insert into MEASUREMENT (measurement_key, timestamp, temperature, sky) values('123456789','2020-12-09 23:59:00',12.0,1);
