1b74adce1e9775ac430717a00d16c098d01c3dc3
yodiack <pipp.peter@gmx.at>
2020-12-19 16:15:27 +0100

    remove code smell

--- src/main/java/at/aau/projects/weatherreporter/rest/RestExceptionHandler.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/RestExceptionHandler.java
@@ -2,7 +2,6 @@ package at.aau.projects.weatherreporter.rest;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +14,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(HttpClientErrorException.class)
     protected ResponseEntity<Object> handleServiceValidationException(HttpClientErrorException ex) {
-        HttpStatus status = ex.getStatusCode() != null ? ex.getStatusCode() : HttpStatus.BAD_REQUEST;
-        return ResponseEntity.status(status).body(ex.getLocalizedMessage());
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getLocalizedMessage());
     }
 }
