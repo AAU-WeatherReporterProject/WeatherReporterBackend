ff09f7c777f7756769dcbb34fa6ad73f16450b58
yodiack <pipp.peter@gmx.at>
2021-01-14 17:02:02 +0100

    now return enum value instead of enum name

--- src/main/java/at/aau/projects/weatherreporter/rest/model/SkyState.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/model/SkyState.java
@@ -1,9 +1,24 @@
 package at.aau.projects.weatherreporter.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SkyState {
-    SUNNY,
-    WINDY,
-    CLOUDY,
-    RAIN,
-    CLEAR,
+    SUNNY("0"),
+    WINDY("1"),
+    CLOUDY("2"),
+    RAIN("3"),
+    CLEAR("4");
+
+    private String skyStateCode;
+    SkyState(String skyStateCode)
+    {
+        this.skyStateCode = skyStateCode;
+    }
+
+    @JsonValue
+    public String getSkyStateCode()
+    {
+        return this.skyStateCode;
+    }
 }
