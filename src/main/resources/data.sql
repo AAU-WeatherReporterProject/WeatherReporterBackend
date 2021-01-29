5101d76ad3939d9eb682e9c4dc6babf128bc37a8
yodiack <pipp.peter@gmx.at>
2021-01-29 20:21:42 +0100

    add the two optinal parameter humidity and pressure

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,6 @@ insert into MEASUREMENT (location, timestamp, temperature, sky) values('TestLoca
 insert into MEASUREMENT (location, timestamp, temperature, sky) values('TestLocation','2020-11-09 23:59:00',12.0,1);
 insert into MEASUREMENT (location, timestamp, temperature, sky) values('TestLocation','2020-10-09 23:59:00',12.0,1);
 insert into MEASUREMENT (location, timestamp, temperature, sky) values('TestLocation','2020-09-09 23:59:00',12.0,1);
-insert into MEASUREMENT (location, timestamp, temperature, sky) values('TestLocation','2020-08-09 23:59:00',12.0,1);
+insert into MEASUREMENT (location, timestamp, temperature,humidity,pressure, sky) values('TestLocation','2020-08-09 23:59:00',12.0,80,980.21,1);
+insert into MEASUREMENT (location, timestamp, temperature,humidity,pressure, sky) values('TestLocation','2020-07-09 23:59:00',12.0,98,1013.25,1);
+insert into MEASUREMENT (location, timestamp, temperature,humidity,pressure, sky) values('TestLocation','2020-06-09 23:59:00',12.0,72,999.11,1);
