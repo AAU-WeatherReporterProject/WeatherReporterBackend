b391a850652dd6b4a48c1e1ba5d8c3ebdb45028d
David Laubreiter <david.laubreiter@outlook.com>
2021-02-13 13:42:02 +0100

    Allow cross origin

--- src/main/java/at/aau/projects/weatherreporter/rest/RestEndpointController.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/RestEndpointController.java
@@ -7,6 +7,7 @@ import at.aau.projects.weatherreporter.rest.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1")
+@CrossOrigin("*")
 public class RestEndpointController {
 
     @Autowired
