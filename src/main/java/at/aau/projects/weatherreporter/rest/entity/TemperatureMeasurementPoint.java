c30efe1d9fa51c900ba9d08d9053ee7ad5e78aea
yodiack <pipp.peter@gmx.at>
2021-02-13 21:19:13 +0100

    reduce length of location to 180

--- src/main/java/at/aau/projects/weatherreporter/rest/entity/TemperatureMeasurementPoint.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/entity/TemperatureMeasurementPoint.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
+import javax.persistence.*;
 
 import java.util.Set;
 
@@ -21,6 +16,7 @@ import java.util.Set;
 public class TemperatureMeasurementPoint {
 
     @Id
+    @Column(length = 180)
     private String location;
     @OneToMany(
             mappedBy = "temperatureMeasurementPoint",
