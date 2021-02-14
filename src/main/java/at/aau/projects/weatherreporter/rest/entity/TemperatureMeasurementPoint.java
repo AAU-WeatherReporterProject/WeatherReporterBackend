1ceb8b5c8546d4daa6bfa67934b5fc029f4db621
yodiack <pipp.peter@gmx.at>
2021-02-14 11:57:00 +0100

    fix checkstyle error

--- src/main/java/at/aau/projects/weatherreporter/rest/entity/TemperatureMeasurementPoint.java
+++ src/main/java/at/aau/projects/weatherreporter/rest/entity/TemperatureMeasurementPoint.java
@@ -4,7 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import java.util.Set;
 
